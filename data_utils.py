46174d31fff8aad30962db3ed15a7885ff1895cf
Lucas Hideki Ueda <lucashueda@gmail.com>
2023-07-28 16:30:58 -0300

    adding energy f0

--- data_utils.py
+++ data_utils.py
@@ -8,7 +8,7 @@ import torch.utils.data
 import qvc_utils
 import commons 
 from mel_processing import spectrogram_torch, spec_to_mel_torch
-from utils import load_wav_to_torch, load_filepaths_and_text, transform
+from qvc_utils import load_wav_to_torch, load_filepaths_and_text, transform
 #import h5py
 
 
