d0f18cdab10730523c9b562edd6c19300de591df
Lucas Hideki Ueda <lucashueda@gmail.com>
2023-07-28 22:23:15 -0300

    adding energy f0

--- train.py
+++ train.py
@@ -203,6 +203,7 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
       y_d_hat_r, y_d_hat_g, fmap_r, fmap_g = net_d(y, y_hat)
       with autocast(enabled=False):
         #loss_dur = torch.sum(l_length.float())
+        print(y_mel.shape, y_hat_mel.shape)
         loss_mel = F.l1_loss(y_mel, y_hat_mel) * hps.train.c_mel
         loss_kl = kl_loss(z_p, logs_q, m_p, logs_p, z_mask) * hps.train.c_kl
 
